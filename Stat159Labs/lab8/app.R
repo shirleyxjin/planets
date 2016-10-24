1e3af19665d4107e167790d7169843866acf1cb7
Shirley Jin <x.jin@berkeley.edu>
2016-10-24 10:31:48 -0700

    changse to app.R

--- Stat159Labs/lab8/app.R
+++ Stat159Labs/lab8/app.R
@@ -1,7 +1,6 @@
-palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
-
 library(shiny)
+
+Advertising <- read.csv("Advertising.csv")
 
 ui <- fluidPage(
   headerPanel('Sales vs. Advertising Budgets'),
